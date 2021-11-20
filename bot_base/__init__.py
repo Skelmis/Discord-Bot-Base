9aacddc8761fb15dc29a7bedeff1925ae2cece2f
Ethan <ethan@koldfusion.xyz>
2021-11-21 01:27:00 +1300

    Up version

--- bot_base/__init__.py
+++ bot_base/__init__.py
@@ -2,11 +2,11 @@ from .bot import BotBase
 import logging
 from collections import namedtuple
 
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(
-    major=1, minor=2, micro=4, releaselevel="production", serial=0
+    major=1, minor=2, micro=5, releaselevel="production", serial=0
 )
