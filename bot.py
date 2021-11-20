6430b45b26c0c8dd43203dec4affafb4cf7ea0b8
Ethan <ethan@koldfusion.xyz>
2021-11-21 00:48:49 +1300

    Add converter support for Document class

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import logging
 import os
 
-
 from bot_base import BotBase
 
 bot = BotBase(
--- bot_base/db/document.py
+++ bot_base/db/document.py
@@ -1,53 +1,85 @@
-from typing import List, Dict, Optional, Union, Any
+import functools
+from typing import List, Dict, Optional, Union, Any, TypeVar, Type
 
 from motor.motor_asyncio import AsyncIOMotorDatabase  # type: ignore
 from pymongo.results import DeleteResult  # type: ignore
 
+T = TypeVar("T")
+
+
+def return_converted(func):
+    @functools.wraps(func)
+    async def wrapped(*args, **kwargs):
+        data: Union[Dict, List[Dict]] = await func(*args, **kwargs)
+
+        self: Document = args[0]
+        if not self.converter:
+            return data
+
+        if not isinstance(data, list):
+            return self.converter(**data)
+
+        new_data = []
+        for d in data:
+            new_data.append(self.converter(**d))
+
+        return new_data
+
+    return wrapped
+
 
 class Document:
-    _version = 6.1
+    _version = 7
 
-    def __init__(self, database: AsyncIOMotorDatabase, document_name: str):
+    def __init__(
+        self, database: AsyncIOMotorDatabase, document_name: str, converter=None
+    ):
         self._document_name: str = document_name
         self._database: AsyncIOMotorDatabase = database
         self.document = database[document_name]
 
+        self.converter: Type[T] = converter
+
     # <-- Pointer Methods -->
-    async def find(self, data_id: str) -> Dict[str, Any]:
+    async def find(self, data_id: Any) -> Union[Dict[str, Any], Type[T]]:
         return await self.find_by_id(data_id)
 
-    async def delete(self, data_id: str) -> Optional[DeleteResult]:
+    async def delete(self, data_id: Any) -> Optional[DeleteResult]:
         return await self.delete_by_id(data_id)
 
     async def update(self, data: Dict[str, Any], *args: Any, **kwargs: Any) -> None:
         await self.update_by_id(data, *args, **kwargs)
 
     # <-- Actual Methods -->
+    @return_converted
     async def get_all(
         self, filter_dict: Optional[Dict[str, Any]] = None, *args: Any, **kwargs: Any
-    ) -> List[Optional[Dict[str, Any]]]:
+    ) -> List[Optional[Union[Dict[str, Any], Type[T]]]]:
         filter_dict = filter_dict or {}
 
         return await self.document.find(filter_dict, *args, **kwargs).to_list(None)  # type: ignore
 
-    async def find_by_id(self, data_id: str) -> Dict[str, Any]:
+    @return_converted
+    async def find_by_id(self, data_id: Any) -> Union[Dict[str, Any], Type[T]]:
         return await self.document.find_one({"_id": data_id})  # type: ignore
 
+    @return_converted
     async def find_by_custom(
         self, filter_dict: Dict[str, Any]
-    ) -> Optional[Dict[str, Any]]:
+    ) -> Optional[Union[Dict[str, Any], Type[T]]]:
         self.__ensure_dict(filter_dict)
 
         return await self.document.find_one(filter_dict)  # type: ignore
 
+    @return_converted
     async def find_many_by_custom(
         self, filter_dict: Dict[str, Any]
-    ) -> List[Dict[str, Any]]:
+    ) -> List[Union[Dict[str, Any], Type[T]]]:
         self.__ensure_dict(filter_dict)
 
         return await self.document.find(filter_dict).to_list(None)  # type: ignore
 
-    async def delete_by_id(self, data_id: str) -> Optional[DeleteResult]:
+    async def delete_by_id(self, data_id: Any) -> Optional[DeleteResult]:
         if await self.find_by_id(data_id) is None:
             return None
 
@@ -130,7 +162,7 @@ class Document:
         await self.document.update_one({"_id": data_id}, {"$unset": data})
 
     async def increment(
-        self, data_id: str, amount: Union[int, float], field: str
+        self, data_id: Any, amount: Union[int, float], field: str
     ) -> None:
         if await self.find_by_id(data_id) is None:
             return
