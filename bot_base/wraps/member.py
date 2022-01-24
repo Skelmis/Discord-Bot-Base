d3e92afd4812fa0e78a6ffae32ad7054d2cb9ebd
Ethan <ethan@koldfusion.xyz>
2022-01-25 01:00:06 +1300

    Fix types

--- bot_base/wraps/member.py
+++ bot_base/wraps/member.py
@@ -1,15 +1,15 @@
-import nextcord as discord
+import nextcord
 from nextcord.ext import commands
 
 from bot_base.wraps.meta import Meta
 
 
-class WrappedMember(Meta, discord.Member):
+class WrappedMember(Meta, nextcord.Member):
     """Wraps discord.Member for ease of stuff"""
 
     @classmethod
     async def convert(cls, ctx, argument: str) -> "WrappedMember":
-        member: discord.Member = await commands.MemberConverter().convert(
+        member: nextcord.Member = await commands.MemberConverter().convert(
             ctx=ctx, argument=argument
         )
         return cls(member, ctx.bot)
--- bot_base/wraps/user.py
+++ bot_base/wraps/user.py
@@ -1,15 +1,15 @@
-import nextcord as discord
+import nextcord
 from nextcord.ext import commands
 
 from bot_base.wraps.meta import Meta
 
 
-class WrappedUser(Meta, discord.User):
+class WrappedUser(Meta, nextcord.User):
     """Wraps discord.user for ease of stuff"""
 
     @classmethod
     async def convert(cls, ctx, argument: str) -> "WrappedUser":
-        user: discord.User = await commands.UserConverter().convert(
+        user: nextcord.User = await commands.UserConverter().convert(
             ctx=ctx, argument=argument
         )
         return cls(user, ctx.bot)
