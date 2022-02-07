9e2cb6adf465ffb928e4bf43d713b188c4ee4376
skelmis <skelmis.craft@gmail.com>
2022-02-07 23:49:24 +1300

    Up version

--- bot_base/__init__.py
+++ bot_base/__init__.py
@@ -5,15 +5,15 @@ from .bot import BotBase
 from .context import BotContext
 from .exceptions import *
 
-__version__ = "1.3.23"
+__version__ = "1.4.0"
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(
     major=1,
-    minor=3,
-    micro=23,
+    minor=4,
+    micro=0,
     releaselevel="production",
     serial=0,
 )
