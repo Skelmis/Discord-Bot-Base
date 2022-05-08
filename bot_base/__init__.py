46fe615c5088ac24206c39c2c6d099fe482a5ca5
skelmis <ethan@koldfusion.xyz>
2022-05-08 22:31:01 +1200

    Up version

--- bot_base/__init__.py
+++ bot_base/__init__.py
@@ -6,7 +6,7 @@ from .cancellable_wait_for import CancellableWaitFor
 from .bot import BotBase
 from .context import BotContext
 
-__version__ = "1.4.6"
+__version__ = "1.4.7"
 
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
@@ -14,7 +14,7 @@ VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
 version_info = VersionInfo(
     major=1,
     minor=4,
-    micro=6,
+    micro=7,
     releaselevel="production",
     serial=0,
 )
