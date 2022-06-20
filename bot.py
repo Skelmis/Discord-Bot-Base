bcbe8cbe75fa4a4646ce66d3cdd8176166eeaae8
skelmis <skelmis.craft@gmail.com>
2022-06-20 15:30:40 +1200

    chore: Undo bot testing code

--- bot.py
+++ bot.py
@@ -1,19 +1,13 @@
-import asyncio
-import contextlib
-import io
-import logging
 import os
-import textwrap
-from traceback import format_exception
+import asyncio
+import logging
 
 import disnake as disnake
-from disnake.ext import commands
 
 from bot_base import BotBase
-from bot_base.paginators.disnake_paginator import DisnakePaginator
 
+log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-logging.getLogger("bot_base.cogs.invite_tracking").setLevel(logging.DEBUG)
 
 
 async def main():
@@ -22,12 +16,13 @@ async def main():
         mongo_url=os.environ["MONGO_URL"],
         mongo_database_name="my_bot",
         load_builtin_commands=True,
+        load_invite_tracking=True,
         intents=disnake.Intents.all(),
     )
 
     @bot.event
     async def on_ready():
-        print("I'm up.")
+        log.info("I'm up")
 
     @bot.command()
     async def echo(ctx):
@@ -44,62 +39,6 @@ async def main():
     async def ping(ctx):
         await ctx.send_basic_embed("Pong!")
 
-    def clean_code(content):
-        """Automatically removes code blocks from the code."""
-        # remove ```py\n```
-        if content.startswith("```") and content.endswith("```"):
-            return "\n".join(content.split("\n")[1:])[:-3]
-        else:
-            return content
-
-    @bot.command(name="eval", aliases=["exec"])
-    @commands.is_owner()
-    async def _eval(ctx, *, code):
-        """
-        Evaluates given code.
-        """
-        code = clean_code(code)
-
-        local_variables = {
-            "commands": commands,
-            "bot": bot,
-            "ctx": ctx,
-            "channel": ctx.channel,
-            "author": ctx.author,
-            "guild": ctx.guild,
-            "message": ctx.message,
-        }
-
-        stdout = io.StringIO()
-
-        try:
-            with contextlib.redirect_stdout(stdout):
-                exec(
-                    f"async def func():\n{textwrap.indent(code, '    ')}",
-                    local_variables,
-                )
-
-                obj = await local_variables["func"]()
-                result = f"{stdout.getvalue()}\n-- {obj}\n"
-
-        except Exception as e:
-            result = "".join(format_exception(e, e, e.__traceback__))
-
-        async def format_page(code, page_number):
-            embed = disnake.Embed(title=f"Eval for {ctx.author.name}")
-            embed.description = f"```{code}```"
-
-            embed.set_footer(text=f"Page {page_number}")
-            return embed
-
-        paginator: DisnakePaginator = DisnakePaginator(
-            1,
-            [result[i : i + 2000] for i in range(0, len(result), 2000)],
-        )
-        paginator.format_page = format_page
-        await paginator.start(context=ctx)
-
-    bot.load_extension("bot_base.cogs.invite_tracking")
     await bot.start(os.environ["TOKEN"])
 
 
